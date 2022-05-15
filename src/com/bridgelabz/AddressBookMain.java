bb86824c152b3dd794dc90cd21693e5cf8ac1079
niludarekar1 <nilu.darekar1@gmail.com>
2022-05-15 17:51:48 +0530

    Added: New Contacts

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,8 +1,44 @@
 package com.bridgelabz;
 
 public class AddressBookMain {
+
     public static void main(String[] args) {
         //Welcome message for Users
-        System.out.println("Welcome to Address Book System Problem Prepared By Nilesh Darekar.");
+        System.out.println("Welcome to Address Book System Problem By Nilesh Darekar.");
+
+        //Variable for user Choice
+        int choice;
+
+        //AddressBook Class Object for Contact Management
+        AddressBook addressBook = new AddressBook();
+
+        //Show Menu for user to Select Operation on AddressBook
+        do {
+            System.out.println("***** ADDRESS BOOK MANAGEMENT *****");
+            System.out.println("1. ADD NEW CONTACT\n2. EDIT CONTACT\n3. DELETE CONTACT" +
+                    "\n4. DISPLAY CONTACT\n5. EXIT");
+            System.out.println("Please Select the Operation Number : ");
+            choice = addressBook.sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    addressBook.addNewContact();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    addressBook.displayContact();
+                    break;
+                case 5:
+                    System.out.println("Thank You for Using Address Book.");
+                    break;
+                default:
+                    System.out.println("Please Select the Operation between 1 to 5 only.");
+                    break;
+            }
+        }while( choice != 5 );
+
     }
 }
