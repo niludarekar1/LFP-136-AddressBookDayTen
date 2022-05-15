7ba07ae7d58c8ce2c65c06fd4090a750bdf071df
niludarekar1 <nilu.darekar1@gmail.com>
2022-05-15 17:42:08 +0530

    Added:Added New Contacts

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        //Welcome message for Users
+        System.out.println("Welcome to Address Book System Problem Prepared By Nilesh Darekar.");
+    }
+}
